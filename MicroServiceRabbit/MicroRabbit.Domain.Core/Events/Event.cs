7d6069517c65445c07f1f5f5fff275357cd3f374
Manish Dubey <mandeep.mca2007@gmail.com>
2025-12-27 21:44:49 +0530

    Implemented Interfaces and classes for Domain.Core project

---
+++ MicroServiceRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; private set; }
+        protected Event()
+        {
+            Timestamp = DateTime.UtcNow;
+        }
+    }
+}
---
+++ MicroServiceRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MessageType { get; private set; }
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
